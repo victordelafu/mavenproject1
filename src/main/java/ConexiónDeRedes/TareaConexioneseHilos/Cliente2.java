c390d2bccda3dd1e446a3ee09712d1b7fb0524f3
Victor de la Fuente <160121458+victordelafu@users.noreply.github.com>
2025-01-27 12:51:19 +0100

    s

--- src/main/java/ConexiónDeRedes/TareaConexioneseHilos/Cliente2.java
+++ src/main/java/ConexiónDeRedes/TareaConexioneseHilos/Cliente2.java
@@ -4,10 +4,26 @@
  */
 package ConexiónDeRedes.TareaConexioneseHilos;
 
+import java.io.*;
+import java.net.*;
 /**
  *
  * @author Alumno
  */
 public class Cliente2 {
-    
-}
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("localhost",1234);
+        
+          BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+          PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+          BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
+          
+          String userInput;
+          while((userInput = teclado.readLine())!=null){
+              out.println(userInput);
+              System.out.println("Servidor: "+in.readLine());
+          }
+          socket.close();
+    }
+    }
+
